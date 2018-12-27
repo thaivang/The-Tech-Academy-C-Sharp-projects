7f5a95b7cb3ec292312632d29c7776188cf71f4a
Thai Vang <thaivangxd@gmail.com>
2018-12-27 10:59:59 -0800

    Add Files

--- AccountingSystem/Program.cs
+++ AccountingSystem/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace AccountingSystem
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting” on you!");
+            Console.ReadLine();
         }
     }
 }
+
