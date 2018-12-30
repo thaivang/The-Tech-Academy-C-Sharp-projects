81b9bf27826b4cb7b536dda38de972759c83ff8d
Thai Vang <thaivangxd@gmail.com>
2018-12-30 11:40:08 -0800

    Add Boolean tutorial

---
+++ Booleanlogic Tutorial/Booleanlogic Tutorial/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booleanlogic_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //And Operator 
+            //Console.WriteLine(true && false); ////false
+            //Console.WriteLine(true && true); //// true
+            //Console.WriteLine(false && false); ////false
+            //Console.WriteLine(false && true); ////false
+
+            ////Or operator
+            //Console.WriteLine(true || true); //true
+            //Console.WriteLine(false || false); //false one of them is true 
+            //Console.WriteLine(true || false); //true 
+            //Console.WriteLine(false || true); //true
+
+            ////!= (does not equal to operator)
+            /*Console.WriteLine(true != true);*/ ////false
+
+            ////x or operator, Branching
+            //Console.WriteLine(true ^ true); // ^ evaluates to true if one of them is true but not both because they are both true 
+            //Console.WriteLine(false ^ false); // is false because both of them is false
+            //Console.WriteLine(true ^ false); // true 
+            //Console.WriteLine(false ^ true); // true
+
+            Console.WriteLine(true && true && true && false);
+
+
+            Console.ReadLine();
+        }
+    }
+}
