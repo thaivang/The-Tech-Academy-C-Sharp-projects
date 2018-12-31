6a0daedc5e31b7f441724f7d21651dc2d731fd0e
Thai Vang <thaivangxd@gmail.com>
2018-12-31 13:48:31 -0800

    Add file

---
+++ CarInsuranceQualification/CarInsuranceQualification/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceQualification
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI? Enter a true or false.");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());           
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedingTicket = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            if (userAge >15 && DUI == false && speedingTicket <= 3)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
