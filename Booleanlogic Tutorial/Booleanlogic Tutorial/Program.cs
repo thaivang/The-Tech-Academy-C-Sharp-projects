f3f7e06c070ccbba37b6527403411db874fd22ad
Thai Vang <thaivangxd@gmail.com>
2018-12-31 13:48:13 -0800

    Update Program.cs

--- Booleanlogic Tutorial/Booleanlogic Tutorial/Program.cs
+++ Booleanlogic Tutorial/Booleanlogic Tutorial/Program.cs
@@ -10,26 +10,26 @@ namespace Booleanlogic_Tutorial
     {
         static void Main(string[] args)
         {
-            //And Operator 
-            //Console.WriteLine(true && false); ////false
-            //Console.WriteLine(true && true); //// true
-            //Console.WriteLine(false && false); ////false
-            //Console.WriteLine(false && true); ////false
+            //And Operator
+            Console.WriteLine(true && false); ////false
+            Console.WriteLine(true && true); //// true
+            Console.WriteLine(false && false); ////false
+            Console.WriteLine(false && true); ////false
 
-            ////Or operator
-            //Console.WriteLine(true || true); //true
-            //Console.WriteLine(false || false); //false one of them is true 
-            //Console.WriteLine(true || false); //true 
-            //Console.WriteLine(false || true); //true
+            //Or operator
+            Console.WriteLine(true || true); //true
+            Console.WriteLine(false || false); //false one of them is true 
+            Console.WriteLine(true || false); //true 
+            Console.WriteLine(false || true); //true
 
-            ////!= (does not equal to operator)
+            //!= (does not equal to operator)
             /*Console.WriteLine(true != true);*/ ////false
 
-            ////x or operator, Branching
-            //Console.WriteLine(true ^ true); // ^ evaluates to true if one of them is true but not both because they are both true 
-            //Console.WriteLine(false ^ false); // is false because both of them is false
-            //Console.WriteLine(true ^ false); // true 
-            //Console.WriteLine(false ^ true); // true
+            //x or operator, Branching
+            Console.WriteLine(true ^ true); // ^ evaluates to true if one of them is true but not both because they are both true 
+            Console.WriteLine(false ^ false); // is false because both of them is false
+            Console.WriteLine(true ^ false); // true 
+            Console.WriteLine(false ^ true); // true
 
             Console.WriteLine(true && true && true && false);
 
