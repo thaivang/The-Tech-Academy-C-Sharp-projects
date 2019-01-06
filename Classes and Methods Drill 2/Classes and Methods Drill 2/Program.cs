a5113f31aaae5ae7d1478cb5cc73149dbeef5f3f
Thai Vang <thaivangxd@gmail.com>
2019-01-06 15:50:30 -0800

    Add drill

---
+++ Classes and Methods Drill 2/Classes and Methods Drill 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_Drill_2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            math mathNumbers = new math(); //created a new object/instance of a class called mathNumbers
+            Console.WriteLine("Passing an integer 12 and adding 10. ");
+            Console.WriteLine(mathNumbers.Number(12)); //calling a method by using class, method name, parameters. Passing 12 as a parameter.
+            Console.WriteLine("Passing a deciaml 5.5 and multiplying by 5. ");
+            Console.WriteLine(mathNumbers.Number(5.5m)); //total is rounded to 30
+            Console.WriteLine("Passing as 15 a string. ");
+            Console.WriteLine(mathNumbers.Number(15)); //total is rounded to 30 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Classes and Methods Drill 2/Classes and Methods Drill 2/math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_Drill_2
+{
+    class math
+    {
+        //methods with same name but must have different data types 
+        public int Number (int addNumber)//create method which will be called from main program
+        {
+            int total1 = addNumber + 10;
+            return total1;
+        }
+        public int Number (decimal multiplyNumber) 
+        {
+            int total2 = Convert.ToInt32(multiplyNumber) * 5;//converting a string value to an int
+            return total2;
+        }
+        public int Number(string divideNumber)
+        {
+            int total2 = Convert.ToInt32(divideNumber) / 10;
+            return total2;
+        }
+    }
+}
