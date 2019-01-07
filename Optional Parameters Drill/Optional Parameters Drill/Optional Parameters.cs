33282e34ff0554d186fa458a29fe3ee514dceb13
Thai Vang <thaivangxd@gmail.com>
2019-01-06 20:22:39 -0800

    Add drill

---
+++ Optional Parameters Drill/Optional Parameters Drill/Optional Parameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Parameters_Drill
+{
+    public class Optional_Parameters
+    {
+        public static int twoParameters (int first, int second = 0) //optional 2nd parameter, set to 0 if user enters only one input
+        {
+            int total = first + second;
+            return total;
+        }
+    }
+}
---
+++ Optional Parameters Drill/Optional Parameters Drill/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optional_Parameters_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Optional_Parameters storNumbers = new Optional_Parameters(); //Instantiate the class object
+            Console.WriteLine("Enter a number: ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number (optional):  or press ENTER to skip and see your total.\n");
+
+            int total; //declare in main in order for "total" to be recognized in context. 
+            var validInput = int.TryParse(Console.ReadLine(), out int input2); //represents a boolean value 
+
+            
+            if (validInput)
+            {
+                total = Optional_Parameters.twoParameters(input1,input2);
+            }
+            else
+            {
+                total = Optional_Parameters.twoParameters(input1);
+            }
+
+            Console.WriteLine("Your total is: "+ total);
+            Console.ReadLine();
+        }
+    }
+}
