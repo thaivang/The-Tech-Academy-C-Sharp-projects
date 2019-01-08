ea5d51394a937f25b14d624f54febfaa338754dd
Thai Vang <thaivangxd@gmail.com>
2019-01-08 11:59:01 -0800

    Add drill

---
+++ Inheritance Drill/Inheritance Drill/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Drill
+{
+    class Employee : Person //inherits from the class person
+    {
+        public int ID { get; set; }
+    }
+}
---
+++ Inheritance Drill/Inheritance Drill/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Drill
+{
+    class Person
+    {
+        public string FirstName { get; set; } //property
+        public string LastName { get; set;} //property
+
+        public void SayName () //superclass method
+        {
+            Console.WriteLine("Name: [{0} {1}]", FirstName, LastName); //prints full name 
+            //Console.ReadLine();
+        }
+    }
+}
