ad0c725865915c3e95dab80c47957fc193c92b7e
Thai Vang <thaivangxd@gmail.com>
2019-01-07 17:36:01 -0800

    Add file

---
+++ Overloading Methods/Overloading Methods/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Methods
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            //1.Create a class. In that class, create a void method that outputs an integer.Have the method divide the data passed to it by 2.
+
+            //2. In the Main() method, instantiate that class.
+
+            //3. Have the user enter a number.Call the method on that number.Display the output to the screen. It should be the entered number, divided by two.
+
+            //4. Create a method with output parameters.
+
+            //5. Overload a method.
+
+            //6. Declare a class to be static.
+
+            overloading_methods favoriteNumber = new overloading_methods(); //object  allows access to nonstatic methods
+            Console.WriteLine("What is your favorite number?");
+            
+            try
+            {
+                int userInput = Convert.ToInt32(Console.ReadLine()); //takes input from user
+                favoriteNumber.divide(userInput); //calls first method by taking user input and divides it by 2 
+                overloading_methods.divide(userInput, out userInput); //calls second method and outputs user input / 2
+                //user input parameter 1 and user input parameter 2
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Enter only an integer.");
+            }
+            Console.ReadLine();
+        }      
+    }
+}
+ 
---
+++ Overloading Methods/Overloading Methods/overloading methods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Methods
+{
+    public class overloading_methods
+    {
+        public void divide (int inNumber) // input of user number
+        {
+            int total =  inNumber / 2;
+            Console.WriteLine(total); //outputs user input /2 to console
+        }
+        public static void divide (int inNumber1, out int outNumber2) //output of user number, overloading method
+        {                                                           //use out parameter to output user input / 2 to screen
+            int total = outNumber2 = inNumber1 /2 ;
+            //Console.WriteLine(total);
+        }
+        public static class empty //declare a static class
+        {
+
+        }
+    }
+}
