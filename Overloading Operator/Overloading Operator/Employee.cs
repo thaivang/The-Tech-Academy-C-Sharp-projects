cc029a49dfb771e0470be5b849c84676431eea3b
Thai Vang <thaivangxd@gmail.com>
2019-01-10 18:03:05 -0800

    Add file

---
+++ Overloading Operator/Overloading Operator/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Operator
+{
+    class Employee
+    {
+        public int id;
+
+        //public override bool Equals(object obj)
+        //{
+        //    Employee e = obj as Employee;
+        //    if (e != null)
+        //    {
+        //        return e == this;
+        //    }
+        //    return false;
+        //}
+
+        //public override int GetHashCode()
+        //{
+        //    return id.GetHashCode();
+        //}
+
+        public static bool operator ==(Employee person1, Employee person2) //overloading operator name 
+        {
+            if (person1.id == person2.id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Employee person1, Employee person2)
+        {
+            return false;
+        }
+    }
+}
---
+++ Overloading Operator/Overloading Operator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an instance of a class and then call the method
+            Employee person1 = new Employee();
+            person1.id = 1;
+            Employee person2 = new Employee();
+            person2.id = 1;
+
+            Console.WriteLine(person1.id==person2.id);
+            Console.ReadLine();
+        }
+    }
+}
