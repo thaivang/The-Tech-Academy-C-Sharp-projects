4aa65e1fc4b0f760f8f5eb7c49adc37d37e4fb19
Thai Vang <thaivangxd@gmail.com>
2019-01-15 12:48:36 -0800

    initial commit

---
+++ File IO Drill/File IO Drill/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_IO_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number of your choosing : ");
+            int text = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C:\number.txt"))//true will append to file otherwise false or without false will create new file(overwriting the previous input)
+            {
+                file.WriteLine(text);
+                file.Close();
+                string file1 = File.ReadAllText(@"C:\number.txt");//file must be closed to read and print to screen
+                Console.WriteLine("Your number is " + file1);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
