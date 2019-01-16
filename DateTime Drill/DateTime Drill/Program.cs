7ca5ad6030abf0c8b88f696d7719c9c27072a562
Thai Vang <thaivangxd@gmail.com>
2019-01-15 18:04:35 -0800

    initial commit

---
+++ DateTime Drill/DateTime Drill/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Prints the current date and time to the console.
+
+            //2.Asks the user for a number.
+
+
+            //3.Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.
+
+            DateTime time = DateTime.Now;             
+            string format = "MMMM d yyyy hh:mm tt"; 
+            Console.WriteLine("Today's date and time is: ");//formatted in -> january(MMMM), 15(d), 2019(yyyy), 6(hh):, mm(00) pm/am (tt)
+            Console.WriteLine(time.ToString(format)); 
+            Console.WriteLine("To find out what time it will be in the future enter a number: ");
+            int hour = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The date and time will be: ");
+            Console.WriteLine(DateTime.Now.AddHours(hour)); //finds out x amount of hours  ahead of current time
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DateTime Drill/DateTime Drill/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DateTime Drill")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("DateTime Drill")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("59b2f24e-51e7-43f2-a41b-15ddff42e98a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
